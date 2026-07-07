186a799a27e15ba14338388932f1fdf0d477efc8
Preethiga03 <preethigaramesh032006@gmail.com>
2026-07-07 21:01:36 +0530

    Added responsive portfolio with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Certifications from "./components/certifications";
-import Footer from"./components/Footer";
- function App(){
-return (
+import Footer from "./components/Footer";
+
+import Hero from "./pages/Hero";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Certifications from "./pages/Certifications";
+
+function App() {
+  return (
     <>
       <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects/>
-      <Certifications/>
-      <Footer/>
+
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/certifications" element={<Certifications />} />
+      </Routes>
+
+      <Footer />
     </>
   );
 }
+
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,20 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
-
-      <div className="logo">
-        Portfolio
-      </div>
+      <div className="logo">Portfolio</div>
 
       <nav>
         <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/skills">Skills</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/certifications">Certifications</Link></li>
         </ul>
       </nav>
-
     </header>
   );
 }
